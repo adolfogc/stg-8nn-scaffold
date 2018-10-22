6ccbe536971f555d018515ab74efd9727c0435a9
Adolfo E. García <adolfo.garcia.cr@gmail.com>
2018-10-22 00:12:09 -0600

    Fix COPYING information

--- bsp/bsp_qpc.c
+++ bsp/bsp_qpc.c
@@ -1,3 +1,23 @@
+/*
+* Copyright (C) 2005-2018 Quantum Leaps, LLC. All rights reserved.
+* Copyright (C) 2018 Adolfo E. García
+*
+* This file is part of STG-8nn-Scaffold.
+*
+* STG-8nn-Scaffold is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* STG-8nn-Scaffold is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with STG-8nn-Scaffold.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/rcc.h>
