6cf108dc41b6b45782d5a29185407df22e787c96
Adolfo E. García <adolfo.garcia.cr@gmail.com>
2018-11-14 17:56:23 -0600

    Fix build

--- ci/build-gcc.sh
+++ ci/build-gcc.sh
@@ -3,7 +3,6 @@
 cd /build && \
 export CC=arm-none-eabi-gcc && \
 cmake -GNinja /src && \
-cmake -GNinja /src && \
 cmake --build . && \
 size firmware.elf && \
 echo "firmware.bin size: `du -h firmware.bin | cut -f1`"
