14d25bbc0b5910b359ee54f9c7f780f886bab227
Adolfo E. García <adolfo.garcia.cr@gmail.com>
2018-11-14 15:43:16 -0600

    Add support for Travis CI

---
+++ ci/build-gcc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /build && \
+export CC=arm-none-eabi-gcc && \
+cmake -GNinja /src && \
+cmake -GNinja /src && \
+cmake --build . && \
+size firmware.elf && \
+echo "firmware.bin size: `du -h firmware.bin | cut -f1`"
