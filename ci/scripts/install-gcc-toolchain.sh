708bfead75d39b166604dbca466b7281b66ee6ca
Adolfo E. García <adolfo.garcia.cr@gmail.com>
2019-01-16 16:26:31 -0600

    Fix path

--- ci/scripts/install-gcc-toolchain.sh
+++ ci/scripts/install-gcc-toolchain.sh
@@ -21,6 +21,6 @@ if [ ! -d ${HOME}/.cache/gcc-arm-none-eabi ]; then
   curl -SOL https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2 && \
   tar xjf gcc-arm-none-eabi-8-2018-q4-major-linux.tar.bz2 && \
   mkdir -p ${HOME}/.cache/gcc-arm-none-eabi && \
-  cp -r ./gcc-arm-none-eabi-8-2018-q4-major-linux/* ${HOME}/.cache/gcc-arm-none-eabi
+  cp -r ./gcc-arm-none-eabi-8-2018-q4-major/* ${HOME}/.cache/gcc-arm-none-eabi
 fi
 
